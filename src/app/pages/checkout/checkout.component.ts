d7db4115b0e9be399231ec7309f98485651afc6d
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-07-14 13:13:29 -0400

    checkout: total  con iva

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -13,8 +13,45 @@ import { RouterModule } from '@angular/router';
 })
 export class CheckoutComponent {
 
-  constructor(){
-    // window.scrollTo(0,0);
+  bandejaList: any[] = [];
+  fechaHoy: string = new Date().toISOString().split('T')[0];
+  randomNum:number = 0;
+  isbandejaList:boolean = false;
+
+  refferalBonouses:number = 2.00;
+  iva:number = 12;
+  
+    constructor() {
+      window.scrollTo(0,0);
+    }
+    ngOnInit(){
+      this.loadBandejaListFromLocalStorage();
+      // this.geneardorOrdeneNumero();
+
+    }
+
+    loadBandejaListFromLocalStorage() {
+    const storedItems = localStorage.getItem('bandejaItems');
+    if (storedItems) {
+      this.bandejaList = JSON.parse(storedItems);
+      
+    }
+    if(this.bandejaList.length > 0){
+      this.isbandejaList = true;
+    }
+  }
+
+  total() {
+    const total = this.bandejaList.reduce((sum, item) => 
+      sum + item.price * item.quantity, 0
+  );
+  return total;
+  }
+
+  totalWithIva() {
+    const baseTotal = this.total();
+    const ivaAmount = baseTotal * this.iva / 100;
+    return baseTotal + ivaAmount;
   }
 
 }
